442e56e02d622184e1d2dfba83d81b4b13651af2
William Seto <seto.william@gmail.com>
2021-10-25 20:26:35 -0400

    feat: add icon

--- src/page-components/results/BackButton.tsx
+++ src/page-components/results/BackButton.tsx
@@ -1,13 +1,19 @@
 import { Button } from "@global-components";
 import { useRouter } from "next/dist/client/router";
 import { FC } from "react";
+import { FaChevronLeft } from "react-icons/fa";
 import styled from "styled-components";
 
 type BackButtonProps = {};
 
 const StyledButton = styled(Button)`
   align-self: flex-start;
+  align-items: center;
   color: rgb(0, 122, 255);
+
+  .icon {
+    color: rgb(0, 122, 255);
+  }
 `;
 
 const BackButton: FC<BackButtonProps> = (props) => {
@@ -15,6 +21,7 @@ const BackButton: FC<BackButtonProps> = (props) => {
 
   return (
     <StyledButton variant="borderless" onClick={() => router.push("/")}>
+      <FaChevronLeft className="icon" />
       Go Back
     </StyledButton>
   );
