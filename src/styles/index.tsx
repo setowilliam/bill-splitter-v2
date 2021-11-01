375cd1ff01fb360694b22aff3d1ae39b5f48f97f
William Seto <seto.william@gmail.com>
2021-11-01 11:39:17 -0400

    feat: try position relative

--- src/styles/index.tsx
+++ src/styles/index.tsx
@@ -8,7 +8,8 @@ export const GlobalStyles = createGlobalStyle`
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
     height: 100vh;
-    overflow-x: hidden;
+    overflow: hidden;
+    position: relative;
     background-color: ${(props) => props.theme.background};
   }
 
