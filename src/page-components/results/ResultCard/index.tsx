92997a39b110fa73a9feb3679e0d36e309acce38
William Seto <seto.william@gmail.com>
2021-11-16 20:42:11 -0500

    feat: add chartjs

--- src/page-components/results/ResultCard/index.tsx
+++ src/page-components/results/ResultCard/index.tsx
@@ -1,20 +1,58 @@
-import { FC } from "react";
+import { FC, useMemo } from "react";
 
-import { Card } from "@global-components";
 import { ResultType } from "utils/typings";
 import Header from "./Header";
 import Body from "./Body";
 import { StyledCard } from "./styles";
+import { getFees, roundNumber } from "utils/functions";
+import { useAtom } from "jotai";
+import { taxAtom, tipAtom, tipTypeAtom } from "@atoms";
+import { globalScope } from "utils/constants";
+import { ChartData } from "chart.js";
+import { Pie } from "react-chartjs-2";
 
 type ResultCardProps = {
   result: ResultType;
 };
 
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
 const ResultCard: FC<ResultCardProps> = (props) => {
+  const [tax] = useAtom(taxAtom, globalScope);
+  const [tip] = useAtom(tipAtom, globalScope);
+  const [tipType] = useAtom(tipTypeAtom, globalScope);
   const { result } = props;
+
+  const pieData: ChartData<"pie"> = useMemo(() => {
+    const data = Object.values(result.resultItems)
+      .sort((a, b) => b.splitPrice - a.splitPrice)
+      .map((resultItem, index) => {
+        const percentage = roundNumber(
+          (resultItem.splitPrice / result.total) * 100,
+          1
+        );
+        const fees = getFees(resultItem.splitPrice, tax, tip, tipType);
+
+        return {
+          name: resultItem.item,
+          value: resultItem.splitPrice + fees.tax + fees.tip,
+        };
+      });
+
+    return {
+      labels: data.map(({ name }) => name),
+      datasets: [
+        {
+          data: data.map(({ value }) => value),
+          backgroundColor: COLORS,
+        },
+      ],
+    };
+  }, [result]);
 
   return (
     <StyledCard header={<Header name={result.name} total={result.total} />}>
+      <Pie data={pieData} />
       <Body resultItems={result.resultItems} total={result.total} />
     </StyledCard>
   );
